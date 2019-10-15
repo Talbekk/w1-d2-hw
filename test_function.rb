cd26b97fecfb6af5032de2fdae03c048ecfcd1f2
Talbekk <mt.campbell@outlook.com>
2019-10-15 16:14:35 +0100

    Ninth Function

--- test_function.rb
+++ test_function.rb
@@ -27,4 +27,33 @@ end
 
 def add_string_as_number(string_1, string_2)
   return string_1.to_i + string_2.to_i
-end 
+end
+
+def number_to_full_name__month_1(num)
+  return case num.to_i
+  when 1
+    "January"
+  when 2
+    "February"
+  when 3
+    "March"
+  when 4
+    "April"
+  when 5
+    "May"
+  when 6
+    "June"
+  when 7
+    "July"
+  when 8
+    "August"
+  when 9
+    "September"
+  when 10
+    "October"
+  when 11
+    "November"
+  else
+    "December"
+  end
+end
