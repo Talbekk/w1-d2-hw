b4d1140a5e9bdb61b95e56cef9d2cd5d77573fbe
Talbekk <mt.campbell@outlook.com>
2019-10-15 16:37:45 +0100

    Thirteenth Function

--- test_function.rb
+++ test_function.rb
@@ -144,3 +144,32 @@ def number_to_short_month_name__month_1(num)
     "Dec"
   end
 end
+
+def number_to_short_month_name__month_4(num)
+  return case num.to_i
+  when 1
+    "Jan"
+  when 2
+    "Feb"
+  when 3
+    "Mar"
+  when 4
+    "Apr"
+  when 5
+    "May"
+  when 6
+    "Jun"
+  when 7
+    "Jul"
+  when 8
+    "Aug"
+  when 9
+    "Sep"
+  when 10
+    "Oct"
+  when 11
+    "Nov"
+  else
+    "Dec"
+  end
+end
